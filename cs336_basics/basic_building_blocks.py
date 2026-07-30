351fe75f7d2a1921c33e6a7c0e64a45ddf8adb9d
Kingbas <1044767717@qq.com>
2026-07-31 01:39:01 +0800

    完成rmsnorm

--- cs336_basics/basic_building_blocks.py
+++ cs336_basics/basic_building_blocks.py
@@ -10,10 +10,10 @@ class Linear(torch.nn.Module):
         super().__init__()
         std = math.sqrt(2/(in_features+out_features))
         W = torch.nn.Parameter(torch.zeros([out_features, in_features], dtype=dtype, device=device))
-        self.W = torch.nn.init.trunc_normal_(W, mean=0, std=std, a=-3*std, b=3*std)
+        self.weight = torch.nn.init.trunc_normal_(W, mean=0, std=std, a=-3*std, b=3*std)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = einsum(x, self.W, '... d_in, d_out d_in -> ... d_out')
+        x = einsum(x, self.weight, '... d_in, d_out d_in -> ... d_out')
         return x
 
 
@@ -21,12 +21,26 @@ class Embedding(torch.nn.Module):
     def __init__(self, num_embeddings, embedding_dim, device=None, dtype=None):
         super().__init__()
         E = torch.nn.Parameter(torch.rand([num_embeddings, embedding_dim], device=device, dtype=dtype))
-        self.E = torch.nn.init.trunc_normal_(E, mean=0, std=1, a=-3, b=3)
+        self.weight = torch.nn.init.trunc_normal_(E, mean=0, std=1, a=-3, b=3)
 
     def forward(self, token_ids: Int[Tensor, " ..."]) -> Float[Tensor, " ... d_model"]:
-        return self.E[token_ids]
-        
-            
+        return self.weight[token_ids]
+
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+        self.weight = torch.nn.Parameter(torch.ones([d_model], dtype=dtype, device=device))
+
+    def forward(self, x: Float[torch.Tensor, 'batch_size sequence_length d_model']) -> Float[torch.Tensor, 'batch_size sequence_length d_model']:
+        original_dtype = x.dtype
+        x = x.to(torch.float32)
+        rms = torch.sqrt(1/self.d_model * torch.square(x).sum(dim=-1, keepdim=True) + self.eps)
+        x = x * self.weight / rms
+        return x.to(original_dtype)
+
 
 if __name__ == '__main__':
     l = Linear(3, 1)
--- tests/adapters.py
+++ tests/adapters.py
@@ -30,7 +30,7 @@ def run_linear(
     """
     from cs336_basics.basic_building_blocks import Linear
     linear = Linear(d_in, d_out)
-    linear.load_state_dict({'W': weights})
+    linear.load_state_dict({'weight': weights})
     return linear(in_features)
 
 
@@ -55,7 +55,7 @@ def run_embedding(
 
     from cs336_basics.basic_building_blocks import Embedding
     embedding = Embedding(vocab_size, d_model)
-    embedding.load_state_dict({'E': weights})
+    embedding.load_state_dict({'weight': weights})
     return embedding(token_ids)
 
 
@@ -383,7 +383,11 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+
+    from cs336_basics.basic_building_blocks import RMSNorm
+    rms_norm = RMSNorm(d_model, eps)
+    rms_norm.load_state_dict({'weight': weights})
+    return rms_norm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
