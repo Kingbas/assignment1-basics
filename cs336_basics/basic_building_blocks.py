815f11b9d73659fcfbbdfc2a4afa8e10e3bc2d70
Kingbas <1044767717@qq.com>
2026-08-01 04:04:43 +0800

    实现swiglu

--- cs336_basics/basic_building_blocks.py
+++ cs336_basics/basic_building_blocks.py
@@ -4,6 +4,7 @@ from torch import Tensor
 from einops import einsum, rearrange
 import math
 from jaxtyping import Bool, Float, Int
+from typing import Any
 
 class Linear(torch.nn.Module):
     def __init__(self, in_features, out_features, device=None, dtype=None):
@@ -20,7 +21,7 @@ class Linear(torch.nn.Module):
 class Embedding(torch.nn.Module):
     def __init__(self, num_embeddings, embedding_dim, device=None, dtype=None):
         super().__init__()
-        E = torch.nn.Parameter(torch.rand([num_embeddings, embedding_dim], device=device, dtype=dtype))
+        E = torch.nn.Parameter(torch.empty([num_embeddings, embedding_dim], device=device, dtype=dtype))
         self.weight = torch.nn.init.trunc_normal_(E, mean=0, std=1, a=-3, b=3)
 
     def forward(self, token_ids: Int[Tensor, " ..."]) -> Float[Tensor, " ... d_model"]:
@@ -42,10 +43,35 @@ class RMSNorm(torch.nn.Module):
         return x.to(original_dtype)
 
 
+class SwiGLU(torch.nn.Module):
+    def __init__(self, d_model, d_ff=None, device=None, dtype=None, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        if d_ff is None:
+            d_ff = int((8/3 * d_model) // 64 * 64)
+        self.w1 = Linear(d_model, d_ff)
+        self.w2 = Linear(d_ff, d_model)
+        self.w3 = Linear(d_model, d_ff)
+
+    def forward(self, x: Float[Tensor, '... d_model']) -> Float[Tensor, '... d_model']:
+        # gate
+        x1 = self.w1(x)
+        x1 = torch.sigmoid(x1) * x1
+        # value
+        x2 = self.w3(x)
+        # output
+        return self.w2(x1 * x2)
+
+
 if __name__ == '__main__':
     l = Linear(3, 1)
     x = torch.rand([3,3])
     weights = torch.rand([1,3])
     out = l(x)
+
+    d_model = 64
+    d_ff = 256
+    swiglu = SwiGLU(d_model)
+    print(list(swiglu.state_dict().keys()))
+    swiglu(torch.rand([1,64]))
     pass
 
